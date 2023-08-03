f89f8db882e0cd8d430fd0e7ad9521e12ca35de6
Pedro Regenes <phregenes@gmail.com>
2023-08-03 17:38:22 -0300

    feat: add count som

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  function handleCounter() {
+    setCount(count + 1);
+  }
+
+  function handleCounter1() {
+    setCount(count - 1);
+  }
+
   return (
     <div className="App">
-      Coming Son...
+      <p>{count}</p>
+      <button onClick={handleCounter}>somar 1</button>
+      <button onClick={handleCounter1}>subitrair 1</button>
     </div>
   );
 }
